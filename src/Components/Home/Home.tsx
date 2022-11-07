51b6e44a40c69e67ecd96932271e928f86c7370f
dewald.labuschagne <dewaldstar@gmail.com>
2022-11-07 10:31:28 +0200

    Spacing and added qoute

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,48 +1,84 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { useEffect, useState } from "react";
 import cwmImg from "./cwmImg.jpeg";
+interface CartoonQuotes {
+  name: string;
+  quote: string;
+  cartoon: string;
+}
 
 export const Home = () => {
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max);
+  };
+
+  const [quote, setQuote] = useState<CartoonQuotes[]>();
+  const [quoteNumber, setQuoteNumber] = useState<number>(getRandomInt(50));
+
+  const fetchCartoon = async () => {
+    await fetch("https://krat.es/6685b8328aa899faddec/cartoonQuotes?limit=100")
+      .then((res) => res.json())
+      .then((result) => {
+        console.log(result);
+        !quote && setQuote(result);
+        setQuoteNumber(getRandomInt(result.length));
+      });
+  };
+  useEffect(() => {
+    !quote && fetchCartoon();
+  }, []);
+
   return (
     <Box>
       <Box sx={styles.paralaxImg}></Box>
       <Box sx={styles.home}>
-        <Typography
-          variant="h3"
-          sx={{
-            backgroundColor: "background.default",
-            textAlign: "center",
-            margin: "20px",
-          }}
-        >
-          Welcome to Carlswald Manor
-        </Typography>
-        <Box sx={styles.homeBox}>
-          <Typography>
-            With a total of 85 units. It's the perfect place to call home or
-            have an investment property. Trafalgar property management is
-            responsible for managing the property and will be the primary
-            contact point for residents should they have queries.
+        <Box>
+          {" "}
+          <Typography
+            variant="h3"
+            sx={{
+              backgroundColor: "background.default",
+              textAlign: "center",
+              margin: "20px",
+            }}
+          >
+            Welcome to Carlswald Manor
           </Typography>
-          <Typography>
-            Carlswald Manor is pet friendly, within reason, pet owners need only
-            register them with the trustees. Residents are held responsible for
-            the care of said pet. Please check whether your pets are allowed
-            before moving in.
+          <Box sx={styles.homeBox}>
+            <Typography>
+              With a total of 85 units. It's the perfect place to call home or
+              have an investment property. Trafalgar property management is
+              responsible for managing the property and will be the primary
+              contact point for residents should they have queries.
+            </Typography>
+            <Typography>
+              Carlswald Manor is pet friendly, within reason, pet owners need
+              only register them with the trustees. Residents are held
+              responsible for the care of said pet. Please check whether your
+              pets are allowed before moving in.
+            </Typography>
+            <Typography>
+              Each unit is fitted with prepaid electricity that is mananged by
+              Prestige metering. It is up to the owner to ensure they are
+              registered with Prestige to avoid disconnection.
+            </Typography>
+            <Typography>
+              The saftey of our residents are our number one priority. Therefore
+              we have 24/7 security with Kallvest that will manage the day to
+              day security needs. For emergency situations BSS security will be
+              contacted for an armed response. More so, our electric perimeter
+              fence immediately triggers an alarm when tampered with.
+            </Typography>
+          </Box>
+        </Box>
+        <Box>
+          {" "}
+          <Typography>{quote && quote[quoteNumber].quote}</Typography>
+          <Typography variant="caption">
+            -{quote && quote[quoteNumber].name} *
+            {quote && quote[quoteNumber].cartoon}
           </Typography>
-          <Typography>
-            Each unit is fitted with prepaid electricity that is mananged by
-            Prestige metering. It is up to the owner to ensure they are
-            registered with Prestige to avoid disconnection.
-          </Typography>
-          <Typography>
-            The saftey of our residents are our number one priority. Therefore
-            we have 24/7 security with Kallvest that will manage the day to day
-            security needs. For emergency situations BSS security will be
-            contacted for an armed response. More so, our electric perimeter
-            fence immediately triggers an alarm when tampered with.
-          </Typography>
-          <Typography>{process.env.REACT_APP_TEST}</Typography>
         </Box>
       </Box>
     </Box>
@@ -53,11 +89,11 @@ const styles = {
   home: {
     textAlign: "center",
     backgroundColor: "background.default",
-    minHeight: "100vh",
+    minHeight: { xs: "100vh", sm: "80vh" },
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    justifyContent: { sm: "start", md: "start" },
+    justifyContent: { xs: "space-between" },
     fontSize: "calc(10px + 2vmin)",
   },
   homeBox: {
@@ -71,8 +107,8 @@ const styles = {
     backgroundImage: `url(${cwmImg})`,
     height: { xs: "150px", sm: "200px" },
     backgroundAttachment: "fixed",
-    backgroundPosition: { xs: "50% 100%", sm: "center" },
+    backgroundPosition: { xs: "49% -10%", sm: "center" },
     backgroundRepeat: "no-repeat",
-    backgroundSize: "cover",
+    backgroundSize: { xs: "700px", sm: "cover" },
   },
 };
